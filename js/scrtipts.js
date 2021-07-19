be690c5472fd83329817c68af27548031fbab7ac
Kiernan2 <kiernan1994@gmail.com>
2021-07-19 15:55:16 -0700

    first commit

---
+++ js/scrtipts.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+  const array1 = ["Vanilla ","Chocolate ","Cookies N' Cream ","Mint Chocolate ","Chocolate Chip Cookie Dough ","Buttered Pecan ","Cookie Dough ","Strawberry ","Moose Tracks ","Neapolitan"]
+  $("#output").append(array1.toString())
+});
