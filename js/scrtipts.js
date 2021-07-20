493a72cfeb8b2c58033a9d9477546da2cbac4a59
Kiernan2 <kiernan1994@gmail.com>
2021-07-20 13:10:44 -0700

    Fixed it so now it uses a loop

--- js/scrtipts.js
+++ js/scrtipts.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
   const array1 = ["Vanilla ","Chocolate ","Cookies N' Cream ","Mint Chocolate ","Chocolate Chip Cookie Dough ","Buttered Pecan ","Cookie Dough ","Strawberry ","Moose Tracks ","Neapolitan"]
-  $("#output").append(array1.toString())
+  array1.forEach(element => {
+    $("#output").append(element)
+  });
 });
